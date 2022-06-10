bca856e490f0632f0af0dceda6c674e458252fdf
Matthew R. Becker <becker.mr@gmail.com>
2022-06-10 16:18:39 -0500

    ENH use rubin str WCS repr to power python one

--- seacliff/rubinwcs.py
+++ seacliff/rubinwcs.py
@@ -1,12 +1,13 @@
 import numpy as np
 
+import lsst.geom
 import lsst.afw.geom
 import galsim
 from galsim.wcs import CelestialWCS
 
 
 class RubinSkyWCS(CelestialWCS):
-    """A galsim-compatible wrapper of the Rubin SkyWCS class.
+    """A galsim-compatible wrapper of the Rubin SkyWcs class.
 
     NOTE: GalSim uses FITS 1-based indexing whereas Rubin uses 0-based indexing. This
     WCS class uses the galsim FITS conventions and explicitly translates via offsets of
@@ -14,24 +15,25 @@ class RubinSkyWCS(CelestialWCS):
 
     Parameters
     ----------
-    rubin_sky_wcs : lsst.afw.geom.SkyWCS
+    rubin_sky_wcs : lsst.afw.geom.SkyWcs
         The Rubin Sky WCS to be wrapped by this class.
     origin : PositionD or PositionI or None, optional
         If not None, the origin position of the image coordinate system. Note that
         the conversion from 1-based to 0-based pixel indexing is **always** done.
     """
-    _req_params = {"rubin_sky_wcs": lsst.afw.geom.SkyWCS}
+    _req_params = {"rubin_sky_wcs": lsst.afw.geom.SkyWcs}
     _opt_params = {"origin": galsim.PositionD}
 
     def __init__(self, rubin_sky_wcs, origin=None):
         self._rubin_sky_wcs = rubin_sky_wcs
+        self._rubin_sky_wcs_str = rubin_sky_wcs.writeString()
         self._set_origin(origin)
         # kept here so that functions in parent class can use it
         self._color = None
 
     @property
     def wcs(self):
-        """the underlying lsst.afw.geom.SkyWCS object"""
+        """the underlying lsst.afw.geom.SkyWcs object"""
         return self._rubin_sky_wcs
 
     @property
@@ -84,9 +86,15 @@ class RubinSkyWCS(CelestialWCS):
         else:
             return x, y
 
+    def copy(self):
+        """make a copy"""
+        return self._newOrigin(self._origin)
+
     def _newOrigin(self, origin):
-        # TODO - figure out how to make a copy?
-        return RubinSkyWCS(self._rubin_sky_wcs, origin=origin)
+        return RubinSkyWCS(
+            self._rubin_sky_wcs.copyAtShiftedPixelOrigin(lsst.geom.Extent2D(0)),
+            origin=origin,
+        )
 
     def __eq__(self, other):
         return (
@@ -95,20 +103,31 @@ class RubinSkyWCS(CelestialWCS):
             (
                 isinstance(other, RubinSkyWCS)
                 and
-                # TODO figure out == for SkyWCS
-                self._rubin_sky_wcs is other._rubin_sky_wcs
+                # same as stack C++
+                # xref: https://github.com/lsst/afw/blob/main/src/geom/SkyWcs.cc#L156
+                self._rubin_sky_wcs_str == other._rubin_sky_wcs_str
                 and
                 self.origin == other.origin
             )
         )
 
-    # TODO add this and eval-able?
-    # def __repr__(self):
-    #     # rely on the repr from Rubin, w/e that is?
-    #     return "seacliff.RubinSkyWCS(%r, origin=%r)" % (
-    #         self._rubin_sky_wcs,
-    #         self.origin,
-    #     )
-    #
-    # def __hash__(self):
-    #     return hash(repr(self))
+    def __repr__(self):
+        return (
+            "seacliff.RubinSkyWCS(lsst.afw.geom.SkyWcs.readString(%r), "
+            "origin=%r)"
+        ) % (
+            self._rubin_sky_wcs_str,
+            self.origin,
+        )
+
+    def __hash__(self):
+        return hash(repr(self))
+
+    def __getstate__(self):
+        d = self.__dict__.copy()
+        del d["_rubin_sky_wcs"]
+        return d
+
+    def __setstate__(self, d):
+        self.__dict__ = d
+        self._rubin_sky_wcs = lsst.afw.geom.SkyWcs.readString(self._rubin_sky_wcs_str)
