8943b00842e98e9579ec32c6bd2218d34511a574
Matthew R. Becker <becker.mr@gmail.com>
2022-06-11 08:59:25 -0500

    PROD fix up metadata

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='seacliff',
-    description="Data-driven simuilations of Rubin Observations",
+    description="data-driven monte carlo simulations of Rubin observations w/ galsim",
     author="Matthew R Becker w/ LSST-DESC",
     packages=find_packages(),
     use_scm_version=True,
