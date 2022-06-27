25822989cfc7e35a7bf2e1a4d57490b114f27602
beckermr <becker.mr@gmail.com>
2022-06-27 15:19:09 -0500

    TST added complete tests

--- seacliff/rubin_noise.py
+++ seacliff/rubin_noise.py
@@ -1,4 +1,11 @@
+import copy
 import numpy as np
+
+import galsim
+from galsim.noise import BaseNoise
+from galsim.random import PoissonDeviate
+
+from seacliff.utils import mad
 
 
 def get_rubin_skyvar_and_gain(calexp):
@@ -75,3 +82,222 @@ def get_rubin_skyvar_and_gain(calexp):
             gn[amp_bbox].array[:, :] = gain
 
     return skyvar.array.copy(), gn.array.copy()
+
+
+class RubinNoise(BaseNoise):
+    """A galsim-compatible noise object that applies the noise from a Rubin calexp to
+    a galsim image.
+
+    The noise model in this class assumes Poisson fluctuations in electrons from both
+    the sky and the objects. The number of electrons in each pixel is computed using
+    the gain from the Rubin calexp. Thus any image to which this class is applied needs
+    to be in ADU or "counts" units that match the Rubin ones. This can be achieved by
+    using the proper zero-point to convert an image in physical units to Rubin counts.
+    It should also not have a background. The output image has the noise applied.
+    To set the variance on a Rubin calexp simulated with this class, use the sky_level
+    and gain attributes like this:
+
+        var = nse.sky_level + im / nse.gain
+
+    where var is the total variance and im is the simulated image in ADU units.
+
+    You can instantiate this object in one of two ways.
+
+        1) Feed it a Rubin calexp:
+
+            >>> nse = seacliff.RubinNoise(calexp)
+
+        2) Feed it a galsim image with the variance (sky level) and gain maps:
+
+            >>> nse = seacliff.RubinNoise(var, gain=gain)
+
+    In all cases the units for the sky level are ADUs and the gain has units e-/ADU.
+
+    Some of the code and methods here were lifted from galsim under its license:
+
+        # Copyright (c) 2012-2021 by the GalSim developers team on GitHub
+        # https://github.com/GalSim-developers
+        #
+        # This file is part of GalSim: The modular galaxy image simulation toolkit.
+        # https://github.com/GalSim-developers/GalSim
+        #
+        # GalSim is free software: redistribution and use in source and binary forms,
+        # with or without modification, are permitted provided that the following
+        # conditions are met:
+        #
+        # 1. Redistributions of source code must retain the above copyright notice, this
+        #    list of conditions, and the disclaimer given in the accompanying LICENSE
+        #    file.
+        # 2. Redistributions in binary form must reproduce the above copyright notice,
+        #    this list of conditions, and the disclaimer given in the documentation
+        #    and/or other materials provided with the distribution.
+        #
+
+    Parameters
+    ----------
+    calexp_or_sky_level :
+    gain :
+    rng :
+    mad_clipping :
+
+    Attributes
+    ----------
+    rng :
+    sky_level :
+    gain :
+    mad_clipping :
+    """
+
+    def __init__(self, calexp_or_sky_level, gain=None, rng=None, mad_clipping=None):
+        super().__init__(rng)
+        self._pd = PoissonDeviate(self.rng)
+        self._mad_clipping = mad_clipping
+
+        if gain is not None:
+            # we got a sky variance and gain
+            self._gn = gain
+            sv = calexp_or_sky_level.array.copy()
+        else:
+            # we got a calexp
+            sv, gn = get_rubin_skyvar_and_gain(calexp_or_sky_level)
+            self._gn = galsim.ImageD(gn)
+
+        if self.mad_clipping is not None:
+            sd, mn = mad(sv, return_median=True)
+            msk = np.abs(sv - mn) > mad_clipping * sd
+            if np.any(msk):
+                sgn = np.sign(sv - mn)
+                sv[msk] = mn + sgn[msk] * mad_clipping * sd
+
+        if gain is not None:
+            _sv = calexp_or_sky_level.copy()
+            _sv.array[:, :] = sv
+            self._sv = _sv
+        else:
+            self._sv = galsim.ImageD(sv)
+
+    @property
+    def mad_clipping(self):
+        """MAD clipping # of sigmas used on the sky level."""
+        return self._mad_clipping
+
+    @property
+    def sky_level(self):
+        """calexp sky level in units of ADU or 'counts'"""
+        return self._sv
+
+    @property
+    def gain(self):
+        """calexp gain in units of e-/ADU"""
+        return self._gn
+
+    def _applyTo(self, image):
+        # the noise array is in ADU
+        noise_array = np.empty(np.prod(image.array.shape), dtype=float)
+        noise_array[:] = image.array.flatten()
+
+        # Minor subtlety for integer images. It's a bit more consistent to convert to an
+        # integer with the sky still added and then subtract off the sky. But this isn't
+        # quite right if the sky has a fractional part.  So only subtract off the
+        # integer part of the sky at the end. For float images, you get the same answer
+        # either way, so it doesn't matter.
+        frac_sky = self.sky_level.array - image.dtype(self.sky_level.array)
+        int_sky = self.sky_level.array - frac_sky
+
+        if np.any(self.sky_level.array != 0):
+            noise_array += self.sky_level.array.flatten()
+
+        # The noise_image now has the expectation values for each pixel with the sky
+        # added but in ADU.
+        # We convert to e-, make the random draw, then convert back.
+        noise_array *= self.gain.array.flatten()
+        noise_array = noise_array.clip(0)  # Make sure no negative values
+        self._pd.generate_from_expectation(noise_array)
+        noise_array /= self.gain.array.flatten()
+
+        # Subtract off the sky, since we don't want it in the final image.
+        if np.any(frac_sky != 0):
+            noise_array -= frac_sky.flatten()
+        # Noise array is now the correct value for each pixel.
+        np.copyto(image.array, noise_array.reshape(image.array.shape), casting="unsafe")
+        if np.any(int_sky != 0):
+            image -= int_sky
+
+    def _getVariance(self):
+        return self.sky_level / self.gain
+
+    def _withVariance(self, variance):
+        if not isinstance(variance, galsim.Image):
+            _variance = self.sky_level.copy()
+            _variance.array[:, :] = variance
+        else:
+            _variance = variance
+
+        return RubinNoise(
+            _variance * self.gain,
+            gain=self.gain,
+            rng=self.rng,
+            mad_clipping=None,
+        )
+
+    def _withScaledVariance(self, variance_ratio):
+        return RubinNoise(
+            self.sky_level * variance_ratio,
+            gain=self.gain,
+            rng=self.rng,
+            mad_clipping=None,
+        )
+
+    def copy(self, rng=None, memo=None):
+        """Make a copy of this noise object.
+
+        By default, the copy will share the `BaseDeviate` random number generator
+        with the RubinNoise from which the copy was made. However, you can provide a
+        new rng for the copy if you'd like.
+
+        Parameters
+        ----------
+        rng :
+
+        Returns
+        -------
+        Copy of this object.
+        """
+        if rng is None:
+            rng = self.rng
+
+        if memo is not None:
+            return RubinNoise(
+                copy.deepcopy(self.sky_level, memo),
+                gain=copy.deepcopy(self.gain, memo),
+                rng=rng,
+                mad_clipping=None,
+            )
+        else:
+            return RubinNoise(
+                copy.deepcopy(self.sky_level),
+                gain=copy.deepcopy(self.gain),
+                rng=rng,
+                mad_clipping=None,
+            )
+
+    def __copy__(self):
+        return self.copy()
+
+    def __deepcopy__(self, memo):
+        return self.copy(memo=memo)
+
+    def __repr__(self):
+        return "seacliff.RubinNoise(%r, gain=%r, rng=%r, mad_clipping=%r)" % (
+            self.sky_level,
+            self.gain,
+            self.rng,
+            None,
+        )
+
+    def __str__(self):
+        return "seacliff.RubinNoise(%r, gain=%r, mad_clipping=%r)" % (
+            self.sky_level,
+            self.gain,
+            None,
+        )
--- tests/test_rubin_noise.py
+++ tests/test_rubin_noise.py
@@ -4,7 +4,8 @@ from numpy.testing import assert_allclose
 
 import galsim
 
-from seacliff.rubin_noise import get_rubin_skyvar_and_gain
+from seacliff.testing import check_pickle_eval_repr_copy
+from seacliff.rubin_noise import get_rubin_skyvar_and_gain, RubinNoise
 import lsst.afw.image
 import lsst.afw.math
 
@@ -53,3 +54,150 @@ def test_rubin_noise_with_gains_dc2():
     exp.setDetector(None)
     sv, gn = get_rubin_skyvar_and_gain(exp)
     assert_allclose(gn, gain, atol=2e-2, rtol=0)
+
+
+def test_rubin_noise_galsim():
+    exp = lsst.afw.image.ExposureD.readFits(
+        os.path.join(os.path.dirname(__file__), "data", "cexp.fits.fz")
+    )
+    exp_bkg = lsst.afw.math.BackgroundList.readFits(
+        os.path.join(os.path.dirname(__file__), "data", "cexp_bkg.fits")
+    )
+    bkg = exp_bkg.getImage().array
+
+    nse = RubinNoise(exp)
+    gain = np.mean([amp.getGain() for amp in exp.getDetector().getAmplifiers()])
+    assert_allclose(nse.gain.array, gain, atol=2e-2, rtol=0)
+    # the bkg = sky counts but variance is sky counts / gain
+    # we check for medians since some regions have huge counts due to artifacts,
+    # saturation etc.
+    assert_allclose(
+        np.median(nse.sky_level.array),
+        np.median(bkg / gain),
+        rtol=0.1,
+        atol=0,
+    )
+
+    assert_allclose(
+        nse.getVariance().array,
+        nse.sky_level.array / nse.gain.array,
+    )
+
+
+def test_rubin_noise_pickle_eval_repr_copy():
+    gain = galsim.ImageD(np.ones((10, 10)) * 0.7)
+    sv = galsim.ImageD(np.ones((10, 10)) * 100)
+
+    nse = RubinNoise(sv, gain=gain)
+    check_pickle_eval_repr_copy(nse)
+
+    # make sure this runs
+    str(nse)
+
+
+def test_rubin_noise_galsim_scale_variance():
+    exp = lsst.afw.image.ExposureD.readFits(
+        os.path.join(os.path.dirname(__file__), "data", "cexp.fits.fz")
+    )
+    gain = np.mean([amp.getGain() for amp in exp.getDetector().getAmplifiers()])
+
+    nse = RubinNoise(exp)
+    new_var = np.median(nse.sky_level.array / nse.gain.array * 4)
+    nse2 = nse.withVariance(new_var)
+    assert_allclose(nse.gain.array, gain, atol=2e-2, rtol=0)
+    assert_allclose(nse2.getVariance().array, new_var)
+
+    nse = RubinNoise(exp)
+    nse2 = nse.withVariance(nse.sky_level / nse.gain * 4)
+    assert_allclose(nse.gain.array, gain, atol=2e-2, rtol=0)
+    assert_allclose(
+        nse2.getVariance().array,
+        4 * nse.sky_level.array / nse.gain.array,
+    )
+
+    nse2 = nse.withScaledVariance(3)
+    assert_allclose(nse.gain.array, gain, atol=2e-2, rtol=0)
+    assert_allclose(
+        nse2.getVariance().array,
+        3 * nse.sky_level.array / nse.gain.array,
+    )
+
+    nse = RubinNoise(exp)
+    nse2 = nse * 4
+    assert_allclose(nse.gain.array, gain, atol=2e-2, rtol=0)
+    assert_allclose(
+        nse2.getVariance().array,
+        4 * nse.sky_level.array / nse.gain.array,
+    )
+
+    nse2 = nse / 3
+    assert_allclose(nse.gain.array, gain, atol=2e-2, rtol=0)
+    assert_allclose(
+        nse2.getVariance().array,
+        nse.sky_level.array / nse.gain.array / 3,
+    )
+
+
+def test_rubin_noise_galsim_mad_clipping():
+    exp = lsst.afw.image.ExposureD.readFits(
+        os.path.join(os.path.dirname(__file__), "data", "cexp.fits.fz")
+    )
+    nse = RubinNoise(exp)
+    nse_clipped = RubinNoise(exp, mad_clipping=5)
+
+    # we should have outliers
+    assert np.abs(nse.sky_level.array).max() > np.abs(nse_clipped.sky_level.array).max()
+
+    # a lot should be the same
+    frac5 = np.mean(nse.sky_level.array == nse_clipped.sky_level.array)
+    assert frac5 > 0.99
+    assert frac5 < 1.00
+
+    # more clipping means less the same
+    nse_clipped = RubinNoise(exp, mad_clipping=1)
+    frac1 = np.mean(nse.sky_level.array == nse_clipped.sky_level.array)
+    assert frac1 < frac5
+
+
+def test_rubin_noise_apply_image():
+    x, y = np.meshgrid(101, 101)
+    gain = galsim.ImageD(0.7 * (1 + y / 100 * 0.1))
+    sv = galsim.ImageD(100 * (1 + x / 100 * 0.1))
+
+    nse = RubinNoise(sv, gain=gain)
+
+    # do a basic check
+    im = galsim.Gaussian(fwhm=2.0).withFlux(1000).drawImage(scale=0.2, nx=101, ny=101)
+    im_orig = im.copy()
+    im.addNoise(nse)
+    assert np.any(im.array != im_orig.array)
+
+    # make sure comes out different
+    im2 = im_orig.copy()
+    im2.addNoise(nse)
+    assert np.any(im2.array != im.array)
+
+    # check values of noise
+    im_orig = (
+        galsim.Gaussian(fwhm=2.0).withFlux(1000).drawImage(scale=0.2, nx=101, ny=101)
+    )
+    arrs = []
+    for _ in range(10_000):
+        im = im_orig.copy()
+        im.addNoise(nse)
+        arrs.append(im.array)
+
+    mn = np.mean(arrs, axis=0)
+    assert_allclose(mn, im_orig.array, atol=1, rtol=0)
+
+    # check seeding
+    nse1 = RubinNoise(sv, gain=gain, rng=galsim.BaseDeviate(10))
+    nse2 = RubinNoise(sv, gain=gain, rng=galsim.BaseDeviate(10))
+    im_orig = (
+        galsim.Gaussian(fwhm=2.0).withFlux(1000).drawImage(scale=0.2, nx=101, ny=101)
+    )
+    im1 = im_orig.copy()
+    im1.addNoise(nse1)
+    im2 = im_orig.copy()
+    im2.addNoise(nse2)
+    assert np.any(im1.array == im2.array)
