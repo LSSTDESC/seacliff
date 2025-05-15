0ba11a75ca6b461bab6c9386ad36c01f0b0a6f99
beckermr <becker.mr@gmail.com>
2025-05-15 12:52:32 -0500

    fix: reorder imports for circular imports and leave a comment

--- seacliff/__init__.py
+++ seacliff/__init__.py
@@ -1,5 +1,8 @@
-from . import config  # noqa: F401
-from ._version import __version__  # noqa: F401
+from ._version import __version__  # noqa: F401, I001
+
 from .rubin_noise import RubinNoise  # noqa: F401
 from .rubin_psf import RubinPSF  # noqa: F401
 from .rubin_sky_wcs import RubinSkyWCS  # noqa: F401
+
+# this has to come last due to potential circular imports
+from . import config  # noqa: F401
