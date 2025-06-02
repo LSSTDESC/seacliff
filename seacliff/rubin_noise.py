584a1ec1278b0951a25851afd396fcc185dd8324
Matthew R. Becker <beckermr@users.noreply.github.com>
2025-06-02 10:31:20 -0500

    feat: use DM routines to get proper gain for removing poisson noise (#10)
    * feat: use DM routines to get proper gain for removing poisson noise
    
    * feat: use robust linear regression
    
    * fix: always seed
    
    * fix: remove extra dep

--- seacliff/rubin_noise.py
+++ seacliff/rubin_noise.py
@@ -4,18 +4,20 @@ import galsim
 import numpy as np
 from galsim.noise import BaseNoise
 from galsim.random import PoissonDeviate
+from lsst.ip.isr.isrFunctions import getExposureGains
+from sklearn.linear_model import RANSACRegressor
 
 from seacliff.utils import mad
 
 
-def get_rubin_skyvar_and_gain(calexp):
-    """Return the map of the sky variance and the gain values from a calexp
+def _lin_fit_helper(x, y):
+    est = RANSACRegressor(random_state=42)
+    est.fit(x.reshape(-1, 1), y.reshape(-1, 1))
+    return est.estimator_.coef_[0], est.estimator_.intercept_[0]
 
-    The algorithm here follows the PR
-    https://github.com/lsst/meas_algorithms/pull/265/files which is based on code in
-    Piff originally AFAIK. We also use the gains as reported by the code if possible
-    following
-    https://github.com/esheldon/metadetect/blob/master/metadetect/lsst/util.py#L239
+
+def get_rubin_skyvar_and_gain(calexp):
+    """Return the map of the sky variance and the gain values from a calexp.
 
     Parameters
     ----------
@@ -29,19 +31,18 @@ def get_rubin_skyvar_and_gain(calexp):
     gain : np.ndarray
         The gain in e-/AUD.
     """
-    try:
-        amps = calexp.getDetector().getAmplifiers()
-        amp_bboxes = [amp.getBBox() for amp in amps]
-    except AttributeError:
-        amp_bboxes = [calexp.getBBox()]
+    gain_dict = getExposureGains(calexp)
 
-    try:
-        amps = calexp.getDetector().getAmplifiers()
-        gains = [amp.getGain() for amp in amps]
-    except AttributeError:
-        gains = None
+    amp_bbox_dict = {}
+    if gain_dict:
+        for amp in calexp.getDetector().getAmplifiers():
+            amp_name = amp.getName()
+            amp_bbox_dict[amp_name] = amp.getBBox()
+    else:
+        amp_name = "seacliff_whole_exposure"
+        amp_bbox = calexp.getBBox()
+        amp_bbox_dict[amp_name] = amp_bbox
 
-    if gains is None or len(gains) != len(amp_bboxes):
         # the gains do not match or are not there so we have to fit for it
         # the fit here is
         #   <var in ADU> = m * <im in ADU> + b
@@ -61,25 +62,28 @@ def get_rubin_skyvar_and_gain(calexp):
         # Finally to get the sky variance we use the image itself and not the fitted sky
         # b:
         #   var - im / gain
-        skyvar = calexp.variance.clone()
-        gn = calexp.variance.clone()
-        for amp_bbox in amp_bboxes:
+        for amp_name, amp_bbox in amp_bbox_dict.items():
             amp_im_arr = calexp.image[amp_bbox].array
             amp_var_arr = calexp.variance[amp_bbox].array
             good = np.isfinite(amp_var_arr) & np.isfinite(amp_im_arr)
-            fit = np.polyfit(amp_im_arr[good], amp_var_arr[good], deg=1)
-            gain = 1.0 / fit[0]
-            skyvar[amp_bbox].array[good] -= amp_im_arr[good] / gain
-            gn[amp_bbox].array[:, :] = gain
-    else:
-        skyvar = calexp.variance.clone()
-        gn = calexp.variance.clone()
-        for amp_bbox, gain in zip(amp_bboxes, gains):
-            amp_im_arr = calexp.image[amp_bbox].array
-            amp_var_arr = calexp.variance[amp_bbox].array
-            good = np.isfinite(amp_var_arr) & np.isfinite(amp_im_arr)
-            skyvar[amp_bbox].array[good] -= amp_im_arr[good] / gain
-            gn[amp_bbox].array[:, :] = gain
+            inv_gain = _lin_fit_helper(amp_im_arr[good], amp_var_arr[good])[0]
+            gain_dict[amp_name] = 1.0 / inv_gain
+
+    skyvar = calexp.variance.clone()
+    gn = calexp.variance.clone()
+    for amp_name, amp_bbox in amp_bbox_dict.items():
+        gain = gain_dict[amp_name]
+
+        amp_im_arr = calexp.image[amp_bbox].array
+        amp_var_arr = calexp.variance[amp_bbox].array
+        good = np.isfinite(amp_var_arr) & np.isfinite(amp_im_arr)
+        skyvar[amp_bbox].array[good] -= amp_im_arr[good] / gain
+
+        bad = (~good) | (skyvar[amp_bbox].array < 0)
+        if np.any(bad):
+            skyvar[amp_bbox].array[bad] = np.median(skyvar[amp_bbox].array[good])
+
+        gn[amp_bbox].array[:, :] = gain
 
     return skyvar.array.copy(), gn.array.copy()
 
